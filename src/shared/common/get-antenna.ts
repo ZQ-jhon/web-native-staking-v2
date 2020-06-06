78143dd22cdc1a997bec661382e027649d50ed17
Tian Pan <puncsky@gmail.com>
2020-06-06 14:30:02 -0700

    new WsSignerPlugin only in browser

--- src/shared/common/get-antenna.ts
+++ src/shared/common/get-antenna.ts
@@ -24,9 +24,10 @@ export function getAntenna(): Antenna {
     return injectedWindow.antenna;
   }
   let signer: SignerPlugin | undefined;
-  signer = new WsSignerPlugin("wss://local.iotex.io:64102");
   if (isIoPay) {
     signer = new WvSigner();
+  } else if (isBrowser) {
+    signer = new WsSignerPlugin("wss://local.iotex.io:64102");
   }
   injectedWindow.antenna = new Antenna("https://api.iotex.one", {
     signer
