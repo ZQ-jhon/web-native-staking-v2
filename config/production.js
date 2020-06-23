f74d68ec3e49b306f42ea5b0339da71dc7683628
Tian Pan <puncsky@gmail.com>
2020-06-23 15:57:40 -0700

    remove v2 in prod and add resources

--- config/production.js
+++ config/production.js
@@ -1,7 +1,6 @@
 module.exports = {
   server: {
     cdnBase: "https://web-native-staking-v2.b-cdn.net/",
-    routePrefix: "/v2",
     cookie: {
       secrets: JSON.parse(
         process.env.COOKIE_SECRETS || '["please specify COOKIE_SECRETS in env"]'
