211cca468ed94d76e068a1b5f662bc0462235ea6
Tian Pan <puncsky@gmail.com>
2020-06-24 01:01:47 -0700

    add production cn

---
+++ config/production-cn.js
@@ -0,0 +1,14 @@
+module.exports = {
+  server: {
+    cookie: {
+      secrets: JSON.parse(
+        process.env.COOKIE_SECRETS || '["please specify COOKIE_SECRETS in env"]'
+      )
+    }
+  },
+  gateways: {
+    logger: {
+      level: "info"
+    }
+  }
+};
