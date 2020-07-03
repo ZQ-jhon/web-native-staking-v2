2015f6addeb64cab6aab99b42a3d96d32d7dcd43
Hung Le <hunglmtb@gmail.com>
2020-07-03 12:54:12 +0700

    add proxy

---
+++ src/server/middleware/iotex-core-proxy-middleware.ts
@@ -0,0 +1,17 @@
+import { Server } from "onefx";
+
+export function setIoTexCoreProxy(server: Server): void {
+  const proxy = require("koa-server-http-proxy");
+  // @ts-ignore
+  let target = server.config.iotexCore;
+  if (target) {
+    target = target.indexOf("http") === -1 ? `http://${target}` : target;
+    server.use(
+      proxy("/iotex-core-proxy/", {
+        target,
+        pathRewrite: { "^/iotex-core-proxy/*": "/" },
+        changeOrigin: true,
+      })
+    );
+  }
+}
--- src/server/middleware/set-middleware.ts
+++ src/server/middleware/set-middleware.ts
@@ -1,6 +1,8 @@
 import { Server } from "onefx/lib/server";
+import { setIoTexCoreProxy } from "./iotex-core-proxy-middleware";
 import { manifestMiddleware } from "./manifest-middleware";
 
 export function setMiddleware(server: Server): void {
   server.use(manifestMiddleware(server));
+  setIoTexCoreProxy(server);
 }
