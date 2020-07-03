5c9bab91c1fa1b665571942f0ff0b95f5f701b6d
Hung Le <hunglmtb@gmail.com>
2020-07-03 13:32:04 +0700

    add proxy (#196)
    * add proxy
    
    * update

--- src/server/middleware/set-middleware.ts
+++ src/server/middleware/set-middleware.ts
@@ -1,6 +1,8 @@
 import { Server } from "onefx/lib/server";
+import { setIoTeXCoreProxy } from "./iotex-core-proxy-middleware";
 import { manifestMiddleware } from "./manifest-middleware";
 
 export function setMiddleware(server: Server): void {
   server.use(manifestMiddleware(server));
+  setIoTeXCoreProxy(server);
 }
