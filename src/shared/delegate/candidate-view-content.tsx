3aae93bcb90afe9ecd47e91772d22a7ceb5fcdd1
Hung Le <hunglmtb@gmail.com>
2020-07-14 17:08:20 +0700

    fix

--- src/shared/delegate/candidate-view-content.tsx
+++ src/shared/delegate/candidate-view-content.tsx
@@ -2,13 +2,16 @@ import RightOutlined from "@ant-design/icons/RightOutlined";
 import Avatar from "antd/lib/avatar";
 import Collapse from "antd/lib/collapse";
 import Layout from "antd/lib/layout";
+// @ts-ignore
+import MarkdownIt from "markdown-it";
 import { t } from "onefx/lib/iso-i18n";
 import { RouteComponentProps } from "onefx/lib/react-router";
 import React, { Component, CSSProperties } from "react";
 import { withRouter } from "react-router";
 import { TBpCandidate } from "../../types";
-import { mdit } from "../article/markdownit";
 import { colors } from "../common/styles/style-color2";
+
+const md = new MarkdownIt();
 
 const TEXT_SIZE = 38;
 const MIDDLE = 0.75;
@@ -99,7 +102,6 @@ class CandidateViewProfileContent extends Component<Props, State> {
   }
 }
 
-const md = mdit;
 // tslint:disable-next-line:no-any
 function getHtml(content: any): { __html: string } {
   return { __html: md.render(content) };
