9b4c21e874137b5067584385a71e83953c1f6caa
Hung Le <hunglmtb@gmail.com>
2020-07-19 15:09:17 +0700

    try csp

--- config/default.js
+++ config/default.js
@@ -43,6 +43,47 @@ module.exports = {
   analytics: {
     googleTid: "TODO: replace with your googleTid",
   },
+  csp: {
+    "default-src": ["*.*"],
+    "manifest-src": ["self", "https://web-native-staking-v2.b-cdn.net/"],
+    "style-src": [
+      "self",
+      "unsafe-inline",
+      "https://fonts.googleapis.com/css",
+      "https://web-native-staking-v2.b-cdn.net/",
+    ],
+    "frame-src": [
+      "*.*"
+    ],
+    "connect-src": [
+      "self",
+      "https://www.google-analytics.com/",
+      "https://member.iotex.io/api-gateway/",
+      "https://web-bp-testnet.herokuapp.com/api-gateway/",
+      "wss://local.iotex.io:64102/",
+      "https://member.iotex.io/iotex-core-proxy/",
+      "https://api.nightly-cluster-2.iotex.one/",
+      "https://api.testnet.iotex.one/",
+      "https://api.iotex.one/",
+      "https://api.cloudinary.com/",
+      "https://analytics.iotexscan.io/query",
+      "https://iotexscan.io/api-gateway/",
+      "https://web-native-staking-v2.b-cdn.net/'",
+      ...(process.env.API_GATEWAY_URL ? [process.env.API_GATEWAY_URL] : []),
+    ],
+    "child-src": ["self"],
+    "font-src": ["self", "data:", "https://fonts.gstatic.com/"],
+    "img-src": ["*", "data:"],
+    "media-src": ["self"],
+    "object-src": ["self"],
+    "script-src": [
+      "self",
+      "https://www.google-analytics.com/",
+      "unsafe-eval",
+      "https://web-native-staking-v2.b-cdn.net/",
+      "https://iotexmember.disqus.com/embed.js",
+    ],
+  },
   apiGatewayUrl:
     process.env.API_GATEWAY_URL || "http://localhost:5004/v2/api-gateway/",
   iotexCore: "https://api.iotex.one",
