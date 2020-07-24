18e7b1c6d9c95c410bba570b3254321a68a0097e
Hung Le <hunglmtb@gmail.com>
2020-07-24 11:53:49 +0700

    add detect chrome version

--- src/server/middleware/set-middleware.ts
+++ src/server/middleware/set-middleware.ts
@@ -1,10 +1,14 @@
 import koa from "koa";
+import { userAgent } from "koa-useragent";
+import UserAgent from "koa-useragent/dist/lib/useragent";
+import { logger } from "onefx/lib/integrated-gateways/logger";
 import { Server } from "onefx/lib/server";
 import { Context, Next } from "onefx/lib/types";
 import { setIoTeXCoreProxy } from "./iotex-core-proxy-middleware";
 import { manifestMiddleware } from "./manifest-middleware";
 
 export function setMiddleware(server: Server): void {
+  server.use(userAgent);
   server.use(async (ctx: Context, next: Next) => {
     await next();
     if (shouldPassCSP(ctx)) {
@@ -23,7 +27,28 @@ export function shouldPassCSP(ctx: koa.Context): boolean {
       (ua && ua.includes("IoPayAndroid")) ||
       ctx.session.app_source === "IoPay"
     ) {
-      return true;
+      const userAgent: UserAgent = ctx.userAgent;
+      if (userAgent && userAgent.isChrome) {
+        const versionText = userAgent.version;
+        if (versionText) {
+          const splits = versionText.split(".");
+          if (splits.length >= 1) {
+            try {
+              const versionNumber = Number(splits[0]);
+              if (versionNumber <= 62) {
+                logger.info(
+                  `detect lower chrome version ${versionText} on android device, will remove csp\nuser-agent ${ua}`
+                );
+                return true;
+              }
+            } catch (e) {
+              logger.info(
+                `detect chrome version ${versionText} on android device, could not parse version number`
+              );
+            }
+          }
+        }
+      }
     }
   }
   return false;
